b4ec18f1224cfc0e9de73078abe61eb6137e9c18
Andrew Playsted <pnkrbt@protonmail.com>
2021-12-03 00:24:07 -0800

    added phaser game into a react component

--- pages/hideout/show.js
+++ pages/hideout/show.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
-import { Grid } from 'semantic-ui-react'
+import { Grid } from 'semantic-ui-react';
+
+import Phaser from 'phaser';
+import { IonPhaser } from '@ion-phaser/react';
+import config from '../../game/game';
 
 import { Link } from '../../routes';
 import web3 from '../../ethereum/web3';
@@ -13,10 +17,16 @@ export default class HideoutShow extends Component {
     };
   }
 
+  state = {
+    initialize: true,
+    game: config
+  }
+
   render() {
-    const {
-      hideout
-    } = this.props;
+    const { hideout } = this.props;
+    const { initialize, game } = this.state
+
+    console.log('game config', config);
 
     return (
       <Layout>
@@ -25,6 +35,7 @@ export default class HideoutShow extends Component {
             <Grid.Row>
               <Grid.Column width={10}>
                 {/* render game */}
+                <IonPhaser game={game} initialize={initialize} />
               </Grid.Column>
               <Grid.Column width={6}>
                 {/* render forms for buying NFTs?
